739fb709707c167e652d9871d08e472fc4c96301
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2024-12-29 21:38:33 +0200

    InputAction and Cinemachine.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,10 +1,13 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
-
+    [SerializeField] InputAction rotation;
+    [SerializeField] float thrustSpeed = 5f;
+    [SerializeField] float rotationSpeed = 5f;
     Rigidbody rb;
 
     private void Start()
@@ -15,13 +18,39 @@ public class Movement : MonoBehaviour
     private void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
     private void FixedUpdate()
     {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+
+        if (rotationInput < 0)
+        {
+            ApplyRotation(rotationSpeed);
+        }
+        else if (rotationInput > 0)
+        {
+            ApplyRotation(-rotationSpeed);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+    }
+
+    private void ProcessThrust()
+    {
         if (thrust.IsInProgress())
         {
-            rb.AddRelativeForce
+            rb.AddRelativeForce(Vector3.up * thrustSpeed * Time.fixedDeltaTime);
         }
     }
 }
