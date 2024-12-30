df5d92b81598d13bbfcc96fc1e373abdf0b85fcb
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2024-12-30 22:02:27 +0200

    SceneLoader

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,20 +1,54 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float loadSceneDilay = 1f;
+
     private void OnCollisionEnter(Collision collision)
     {
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                Debug.Log("You are Frendly state");
-                break;
-            case "Fuel":
-                Debug.Log("You are Fuel state");
+                Debug.Log("You are Starting Platform.");
                 break;
             case "Finish":
-                Debug.Log("You are Finish state");
+                StartSuccessSequense();
+                Debug.Log("FINISH");
+                break;
+            case "Trash":
+                StartCrashSequence();
                 break;
         }
+    }
+
+    void StartSuccessSequense()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", loadSceneDilay);
+    }
+
+    void StartCrashSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", loadSceneDilay);
+    }
+
+    void ReloadLevel()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int nextScene = currentScene + 1;
+        if (nextScene == SceneManager.sceneCountInBuildSettings)
+        {
+            nextScene = 0;
+        }
+        SceneManager.LoadScene(nextScene);
     }
 }
